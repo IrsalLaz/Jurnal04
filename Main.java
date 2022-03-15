f4133293ae9b3741d8311931cf4f16e91d848684
IrsalLaz <91246036+IrsalLaz@users.noreply.github.com>
2022-03-16 00:13:33 +0800

    ifElse no loop added yet

--- Main.java
+++ Main.java
@@ -1,33 +1,73 @@
 import java.util.LinkedList;
 import java.util.ListIterator;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         LinkedList<Song>  playList = new LinkedList<Song>();
-
+        Scanner in = new Scanner(System.in);
+        String penyanyi, judul;
+        
+        //test data
         playList.add(new Song("balonku", "bapakmu"));
         playList.addLast(new Song("monster", "imagine dragon"));
         playList.add(new Song("SHINDEE", "takayan"));
         playList.addFirst(new Song("toy", "takayan"));
 
+        System.out.println( "====================SONG PLAYLIST====================");
+        System.out.println("1. Play \n2. Input song \n3. Delete Song");
+        int choice = in.nextInt();
         //Method playlist
-        ListIterator<Song> iterator = playList.listIterator();
-        
-        System.out.println("print berurut");
-        while (iterator.hasNext()) {
-            System.out.println(iterator.next());
-        }
-        System.out.println("Print mundur");
-        while (iterator.hasPrevious()) {
-            System.out.println(iterator.previous());
-        }
-        
-        playList.remove();
-        System.out.println("Sesudah hapus lagu");
-        for (Song song : playList) {
-            System.out.println(song);
-        }
-        
 
+        ListIterator<Song> iterator = playList.listIterator();
+        if (choice == 1) {
+            System.out.println("Kamu lebih suka mana?\n1. Berurut\n2. Mundur");
+            choice = in.nextInt();
+            if (choice == 1) {
+                System.out.println("========Berurut========");
+                while (iterator.hasNext()){
+                    System.out.println(iterator.next());
+                }
+            }
+            else if(choice == 2){
+                System.out.println("========Mundur========");
+                while (iterator.hasPrevious()){
+                    System.out.println(iterator.previous());
+                }
+            }
+            else{
+                System.out.println("Pilihanmu nggak ada :(");
+            }
+        }
+        else if (choice == 2) {
+            System.out.println("Judul:");
+            judul = in.nextLine();
+            System.out.println("Penyanyi:");
+            penyanyi = in.nextLine();
+
+            Song lagu = (new Song(judul, penyanyi));
+            playList.add(lagu);
+        }
+        else if(choice == 3){
+            System.out.println("Lagu terakhir?\n 1.ya\n2. bukan");
+            choice = in.nextInt();
+            if (choice == 1) {
+                playList.remove();               
+            }
+            else if(choice == 2){
+                System.out.println("Masukan Judul lagu");
+                judul = in.nextLine();
+                
+                while (iterator.hasNext()) {
+                    Song ob = iterator.next();
+                    if (ob.getJudul().equals(judul)) {
+                        iterator.remove();
+                    }
+                }
+            }
+            else{
+                System.out.println("Pilihanmu nggak ada :(");
+            }
+        }
     }
 }
